f04a461e4ce827cb9c6ff6d3729b1216464ed0ee
Max Koriakin <maxsus92@gmail.com>
2019-04-24 09:59:19 +0300

    no message

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Max Koriakin on 4/24/19.
+//  Copyright © 2019 Max Koriakin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
